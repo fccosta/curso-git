a156ad381081d6c968ab7d2a54d5781158a485c3
fccosta <fc_costa@outlook.com.br>
2018-07-08 17:29:43 -0300

    Acrescentando a mensagem tenha um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,8 +9,8 @@ namespace projeto_teste {
         static void Main(string[] args) {
 
             Console.WriteLine("Ola mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
